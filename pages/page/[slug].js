3205b651457c566a0ed9a188c12ed790b02cbd5a
zedonboy <zedonbiz@gmail.com>
2021-08-31 10:18:12 +0100

    Implementation of page creation feature

---
+++ pages/page/[slug].js
@@ -0,0 +1,130 @@
+import Head from "next/head";
+import MainBanner from "../../components/Sections/MainBanner";
+
+import Subscribe from "../../components/Sections/SubscribeForm";
+import AboutUs from "../../components/Sections/AboutUs";
+import ServicesCard from "../../components/Sections/ServicesCard";
+import LatestBlogPost from "../../components/Sections/LatestBlogPost";
+import CTA from "../../components/Sections/CTA";
+import WhatWeDo from "../../components/Sections/WhatWeDo";
+import CapabilitySection from "../../components/Sections/Capability";
+//import PastWork from "../components/Common/PastWork";
+import { createGlobalStyle } from "styled-components";
+import { API_URL } from "../../config/api";
+import CaseStudiesTwo from "../../components/Sections/CaseStudiesTwo";
+import Partner from "../../components/Sections/Partner";
+import Pricing from "../../components/Sections/PricingSection";
+import HowItWork from "../../components/Sections/HowItWork";
+import OurTeamStyleTwo from "../../components/Sections/OurTeamStyleTwo";
+import TestimonialStyleOne from "../../components/Sections/TestimonialStyleOne";
+import Layout from '../../components/_App/Layout';
+import InfoSection from "../../components/Sections/InfoSection";
+import RInfoSection from "../../components/Sections/ReverseInfoSection";
+import SingleServices from "../../components/Sections/ServiceSection";
+import ASingleServices from "../../components/Sections/AdvancedServiceSection";
+
+export default function Home({ site, page, posts }) {
+  let GlobalStyleComponent = null;
+  GlobalStyleComponent = createGlobalStyle`
+  :root {
+    --fontFamily: 'Nunito', sans-serif;
+    --mainColor: ${site?.mainColor || "#fe4c1c"};
+    --optionalColor: ${site?.optionalColor};
+    --whiteColor: #ffffff;
+    --blackColor: ${site?.blackColor || "#080e32"};
+    --fontSize: ${site?.fontSize || "16px"};
+    --transition: .5s;
+    --navLinkColor: #ffffff;
+    scroll-behavior: initial;
+  }
+  
+`;
+  return (
+    <Layout siteData={site}>
+      <Head>
+        <title>{page?.title}</title>
+        {page?.meta?.map((meta) => (
+          <meta property={meta?.property} content={meta?.content}/>
+        ))}
+      </Head>
+      <GlobalStyleComponent />
+      
+      {page?.layout?.map((section) => {
+        switch (section.__component) {
+          case "page-sections.main-banner":
+            return <MainBanner data={section} />;
+          case "page-sections.about-us":
+            return <AboutUs data={section} />;
+          case "page-sections.blog-section":
+            return <LatestBlogPost data={section} posts={posts} />;
+          case "page-sections.capability-section":
+            return <CapabilitySection data={section} />;
+          case "page-sections.card-section":
+            return <ServicesCard data={section} />;
+          case "page-sections.case-section":
+            return <CaseStudiesTwo data={section} />;
+          case "page-sections.cta-section":
+            return <CTA data={section} />;
+          case "page-sections.partner-section":
+            return <Partner data={section} />;
+          case "page-sections.price-section":
+            return <Pricing data={section} />;
+          case "page-sections.step-section":
+            return <HowItWork data={section} />;
+          case "page-sections.subscribe-section":
+            return <Subscribe data={section} />;
+          case "page-sections.team-section":
+            return <OurTeamStyleTwo data={section} />;
+          case "page-sections.testimonial-section":
+            return <TestimonialStyleOne data={section} />;
+          case "page-sections.accordion-section":
+            return <WhatWeDo data={section}/>
+          case "page-sections.info-section":
+            return <InfoSection data={section}/>
+          case "page-sections.rinfo-section":
+            return <RInfoSection data={section}/>
+          case "page-sections.service-section":
+            return <SingleServices data={section}/>
+          case "page-sections.advanced-service-section":
+            return <ASingleServices data={section}/>
+        }
+      })}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+    let slug = ctx.params.slug
+  let resp = await fetch(`${API_URL}/site`, {
+    method: "GET",
+  });
+
+  let respData = await fetch(`${API_URL}/pages?slug=${slug}`, {
+    method: "GET",
+  });
+
+  let postResp = await fetch(`${API_URL}/posts?featured=true`, {
+    method: "GET"
+  })
+
+  if (resp.status === 200 && respData.status === 200) {
+    let site = await resp.json();
+    let pageArr = await respData.json();
+    let page = pageArr[0]
+    let posts = []
+    if(postResp.status === 200) posts = await postResp.json()
+
+    return {
+      props: {
+        site,
+        page,
+        posts
+      },
+    };
+  } else {
+    return {
+      notFound:true,
+      props: {},
+    };
+  }
+}
