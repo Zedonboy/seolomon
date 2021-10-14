dc6b8b0733b74fcb1e038f2729a2127d83d21629
zedonboy <zedonbiz@gmail.com>
2021-10-14 21:40:44 +0100

    correct back to old

--- config/api.js
+++ config/api.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === "production" ? process.env.SERVER_URL : "http://localhost:1337"
+export const API_URL = process.env.NODE_ENV === "production" ? "https://seolomon.herokuapp.com" : "http://localhost:1337"
