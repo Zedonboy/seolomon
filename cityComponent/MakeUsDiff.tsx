3eec60d2feeb950390dbfd1bea1341b3c92ba967
zedonboy <zedonbiz@gmail.com>
2021-12-26 16:21:50 +0100

    update

--- cityComponent/MakeUsDiff.tsx
+++ cityComponent/MakeUsDiff.tsx
@@ -26,7 +26,7 @@ export default function MakeUsDiff(props : {data : IMakeUsDiff}) {
     <div className="py-16 bg-gray-50 overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
         <div className="text-base max-w-prose mx-auto lg:max-w-none">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+          <h2 className="text-base text-green-600 font-semibold tracking-wide uppercase">
             Automated
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
@@ -42,14 +42,14 @@ export default function MakeUsDiff(props : {data : IMakeUsDiff}) {
           <div className="relative z-10">
             <div dangerouslySetInnerHTML={{
               __html: props?.data?.message
-            }} className="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
+            }} className="prose prose-green text-gray-500 mx-auto lg:max-w-none">
               
             </div>
             {/* <div className="mt-10 flex text-base max-w-prose mx-auto lg:max-w-none">
               <div className="rounded-md shadow">
                 <a
                   href="#"
-                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700"
                 >
                   Contact sales
                 </a>
@@ -57,7 +57,7 @@ export default function MakeUsDiff(props : {data : IMakeUsDiff}) {
               <div className="rounded-md shadow ml-4">
                 <a
                   href="#"
-                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
+                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-green-600 bg-white hover:bg-gray-50"
                 >
                   Learn more
                 </a>
@@ -100,11 +100,11 @@ export default function MakeUsDiff(props : {data : IMakeUsDiff}) {
             </svg>
             <blockquote className="relative bg-white rounded-lg shadow-lg">
               <div className="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8">
-                <img
-                  src="https://tailwindui.com/img/logos/workcation-logo-indigo-600-mark-gray-800-and-indigo-600-text.svg"
+                {/* <img
+                  src="https://tailwindui.com/img/logos/workcation-logo-green-600-mark-gray-800-and-green-600-text.svg"
                   alt="Workcation"
                   className="h-8"
-                />
+                /> */}
                 <div className="relative text-lg text-gray-700 font-medium mt-8">
                   <svg
                     className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200"
@@ -122,12 +122,12 @@ export default function MakeUsDiff(props : {data : IMakeUsDiff}) {
               <cite className="relative flex items-center sm:items-start bg-green-600 rounded-b-lg not-italic py-5 px-6 sm:py-5 sm:pl-12 sm:pr-10 sm:mt-10">
                 {/* <div className="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
                   <img
-                    className="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-indigo-300"
+                    className="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-green-300"
                     src={props?.data?.main_review?.role}
                     alt=""
                   />
                 </div> */}
-                <span className="relative ml-4 text-indigo-300 font-semibold leading-6 sm:ml-24 sm:pl-1">
+                <span className="relative ml-4 text-green-300 font-semibold leading-6 sm:ml-24 sm:pl-1">
                   <p className="text-white font-semibold sm:inline">
                   {props?.data?.main_review?.name}
                   </p>{" "}
